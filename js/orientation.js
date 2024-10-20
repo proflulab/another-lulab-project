5074702609cffaf0e1ee1e11937624217138e208
jason wang <itsjasonagainst@gmail.com>
2024-10-20 22:01:54 +0800

    Commit message for changes

--- js/orientation.js
+++ js/orientation.js
@@ -1,36 +1,3 @@
-// Orientation detection and warning functionality
-document.addEventListener('DOMContentLoaded', function () {
-    // Function to detect the orientation of the device
-    function detectOrientation() {
-        if (window.innerWidth < window.innerHeight) {
-            showOrientationWarning();
-        } else {
-            hideOrientationWarning();
-        }
-    }
-
-    // Function to show the orientation warning
-    function showOrientationWarning() {
-        const warning = document.getElementById('orientationWarning');
-        if (!warning) {
-            const orientationDiv = document.createElement('div');
-            orientationDiv.id = 'orientationWarning';
-            orientationDiv.innerHTML = '<h1>Please Rotate Your Device</h1>';
-            document.body.appendChild(orientationDiv);
-        }
-    }
-
-    // Function to hide the orientation warning
-    function hideOrientationWarning() {
-        const warning = document.getElementById('orientationWarning');
-        if (warning) {
-            document.body.removeChild(warning);
-        }
-    }
-
-    // Call the orientation detection function after the page loads
-    detectOrientation();
-
-    // Listen for changes in the window size (when the user rotates the device)
-    window.addEventListener('resize', detectOrientation);
-});
+version https://git-lfs.github.com/spec/v1
+oid sha256:053c0f2299c27973563a5c81233a5bc38f0db24afd5ef54653c33d573ae6617e
+size 1301
