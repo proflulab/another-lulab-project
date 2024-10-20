6e94af9c08a76fcb4bde67bac6bf74518e83731e
happysashimi <itsjasonagainst@gmail.com>
2024-10-21 01:09:00 +0800

    Delete js directory

--- js/orientation.js
+++
@@ -1,3 +0,0 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:36adb5729433844397a00275fa002eed8c59f1bd48cdf46c07b3a3578ed95f3f
-size 1335
--- js/script.js
+++
@@ -1,3 +0,0 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:928080ec5210150db9a6c068cc1f286bc4373f645e3ebf3790257754044ee309
-size 8168
